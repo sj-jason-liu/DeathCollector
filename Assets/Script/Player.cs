1ae1c3216e435a1b07911f6eae07ffa344aee04b
sj-jason-liu <sj.jason.liu@gmail.com>
2022-05-22 12:19:07 +0800

    Added Player model from Mixamo & added basic action animation(idle, running, jumping, and double jump)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
+    private PlayerAnimation _anim;
 
     [SerializeField]
     private float _speed = 3f;
@@ -19,6 +20,7 @@ public class Player : MonoBehaviour
 
     private Vector3 _spawnPosition;
 
+    private bool _isJumping;
     private bool _hasDoubleJump;
     
     void Start()
@@ -26,6 +28,9 @@ public class Player : MonoBehaviour
         _controller = GetComponent<CharacterController>();
         if (_controller == null)
             Debug.LogError("Character Controller is NULL!");
+        _anim = GetComponentInChildren<PlayerAnimation>();
+        if (_anim == null)
+            Debug.LogError("Player Animator is NULL!");
 
         _spawnPosition = transform.position;
     }
@@ -67,13 +72,24 @@ public class Player : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(horizontalMove, 0, 0);
         Vector3 velocity = movement * _speed;
+        _anim.Moving(Mathf.Abs(horizontalMove));
 
+        //if(_isJumping)
+        //{
+        //    _isJumping = false;
+        //    _anim.Jumping(_isJumping);
+        //}
+        
         if (_controller.isGrounded)
         {
             _hasDoubleJump = false;
+            _isJumping = false;
+            _anim.Jumping(_isJumping);
             if (Input.GetKeyDown(KeyCode.Space)) //jump
             {
                 _yVelocity = _jumpHeight;
+                _isJumping = true;
+                _anim.Jumping(_isJumping);
             }
         }
         else
@@ -82,6 +98,7 @@ public class Player : MonoBehaviour
             {
                 _yVelocity += _jumpHeight * _doubleJumpAdjust;
                 _hasDoubleJump = true;
+                _anim.DoubleJump();
             }
             else
             {

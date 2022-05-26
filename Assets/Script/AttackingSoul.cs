fec461713e09e4653120bec4542ae52fa0b86767
sj-jason-liu <sj.jason.liu@gmail.com>
2022-05-26 22:25:22 +0800

    Added soul launcher to Player and can actually fire souls.

---
+++ Assets/Script/AttackingSoul.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingSoul : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //move to right
+        transform.Translate(new Vector3(0, 0, _speed * Time.deltaTime));
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable hit = other.GetComponent<IDamageable>();
+        if (hit == null)
+            return;
+        hit.Damage();
+    }
+}
--- Assets/Script/Player/SoulLauncher.cs
+++ Assets/Script/Player/SoulLauncher.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SoulLauncher : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _attackingSoul;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +15,14 @@ public class SoulLauncher : MonoBehaviour
 
     public void LaunchSoul()
     {
+        Invoke("LaunchInvoke", 0.8f);
+    }
+
+    void LaunchInvoke()
+    {
         //instantiate soul
+        Vector3 currentPosition = transform.position;
+        GameObject firedSoul = Instantiate(_attackingSoul, currentPosition, transform.rotation);
+        firedSoul.transform.SetParent(gameObject.transform);
     }
 }
